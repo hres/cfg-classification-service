22c26f6f376d664802c769ba30f7ae31132e6669
rsanchej <raul.sanchez@canada.ca>
2017-04-18 15:12:05 -0400

    added annotations

--- src/main/java/ca/gc/ip346/classification/resource/ClassificationResource.java
+++ src/main/java/ca/gc/ip346/classification/resource/ClassificationResource.java
@@ -1,33 +1,14 @@
 package ca.gc.ip346.classification.resource;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringTokenizer;
 
-import javax.naming.NamingException;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import org.apache.commons.lang.StringUtils;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures;
@@ -35,12 +16,15 @@ import com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures;
 import ca.gc.ip346.classification.model.CanadaFoodGuideDataset;
 import ca.gc.ip346.classification.model.Dataset;
 
+@Path("/")
+@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 public class ClassificationResource {
 
 	@POST
 	@Path("/classify")
-	@Consumes (MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@JacksonFeatures(serializationEnable = {SerializationFeature.INDENT_OUTPUT})
 	public List<List<CanadaFoodGuideDataset>> classifyDataset(@BeanParam Dataset dataset) {
 		List<CanadaFoodGuideDataset> foods = dataset.getData();
