93f78f8f6fc9c0d83a92baca59c40abb6529fc92
rsanchej <raul.sanchez@canada.ca>
2017-05-03 10:29:33 -0400

    calculation per RA for CFG food item

--- src/main/java/ca/gc/ip346/classification/resource/FlagsEngine.java
+++ src/main/java/ca/gc/ip346/classification/resource/FlagsEngine.java
@@ -9,17 +9,17 @@ import org.kie.api.runtime.KieSession;
 
 import ca.gc.ip346.classification.model.CanadaFoodGuideDataset;
 public class FlagsEngine {
-	
+
 	public static FlagsEngine flagsEngine = new FlagsEngine();
 	//The pipeline is used to rule the drools rules one at a time
 	private List<KieSession> kieSessionPipeline;
 	private List<CanadaFoodGuideDataset> foodResults;
-	
+
 	//recreates singleton
 	public static void refreshEngine(){
 		flagsEngine = new FlagsEngine();
 	}
-	
+
 	public FlagsEngine(){
 		//taken from Wei Fang's code
 		KieServices ks = KieServices.Factory.get();
@@ -28,19 +28,19 @@ public class FlagsEngine {
 		//kieSessionPipeline.add(kContainer.newKieSession("ksession-process-flags"));
 		kieSessionPipeline.add(kContainer.newKieSession("ksession-process-refamt"));
 	}
-	
+
 	private void prepare(CanadaFoodGuideDataset food){
-		
+
 		/*
 		Removed due to removing flags table execution
-		
+
 		food.setContainsAddedFat(false);
 		food.setContainsAddedSodium(false);
 		food.setContainsAddedSugar(false);
 		food.setContainsAddedTransfat(false);
 		food.setContainsCaffeine(false);
 		*/
-		
+
 		//This is done as the FIRST prepare ONLY
 		food.setAdjustedReferenceAmount(food.getReferenceAmountG());
 		food.setFopAdjustedReferenceAmount(food.getReferenceAmountG());
@@ -49,28 +49,28 @@ public class FlagsEngine {
 		food.setAbsolute(false);
 		food.setShift(0);
 	}
-	
+
 	public List<CanadaFoodGuideDataset> setFlags(List<CanadaFoodGuideDataset> foods){
 		foodResults = new ArrayList<>();
 		fireDrools(foods);
 		return foodResults;
 	}
-	
+
 	private void calculatePerRA(CanadaFoodGuideDataset food){
-		food.setSodiumPerReferenceAmount(food.getSodiumAmountPer100g()*food.getAdjustedReferenceAmount()/100);
-		food.setSugarPerReferenceAmount(food.getSugarAmountPer100g()*food.getAdjustedReferenceAmount()/100);
-		food.setFatPerReferenceAmount(food.getTotalfatAmountPer100g()*food.getAdjustedReferenceAmount()/100);
-		food.setSatFatPerReferenceAmount(food.getSatfatAmountPer100g()*food.getAdjustedReferenceAmount()/100);
-		food.setTransFatPerReferenceAmount(food.getTransfatAmountPer100g()*food.getAdjustedReferenceAmount()/100);
-		food.setFopSodiumPerReferenceAmount(food.getSodiumAmountPer100g()*food.getFopAdjustedReferenceAmount()/100);
-		food.setFopSugarPerReferenceAmount(food.getSugarAmountPer100g()*food.getFopAdjustedReferenceAmount()/100);
+		food    .setSodiumPerReferenceAmount(food   .getSodiumAmountPer100g() * food    .getAdjustedReferenceAmount() / 100);
+		food     .setSugarPerReferenceAmount(food    .getSugarAmountPer100g() * food    .getAdjustedReferenceAmount() / 100);
+		food       .setFatPerReferenceAmount(food .getTotalfatAmountPer100g() * food    .getAdjustedReferenceAmount() / 100);
+		food    .setSatFatPerReferenceAmount(food   .getSatfatAmountPer100g() * food    .getAdjustedReferenceAmount() / 100);
+		food  .setTransFatPerReferenceAmount(food .getTransfatAmountPer100g() * food    .getAdjustedReferenceAmount() / 100);
+		food .setFopSodiumPerReferenceAmount(food   .getSodiumAmountPer100g() * food .getFopAdjustedReferenceAmount() / 100);
+		food  .setFopSugarPerReferenceAmount(food    .getSugarAmountPer100g() * food .getFopAdjustedReferenceAmount() / 100);
 	}
 
 	/*
 	 * Function: fireDrools
 	 *  Purpose: fire the drools rules and calculate
 	 *       in: foods
-	*/
+	 */
 	private void fireDrools(List<CanadaFoodGuideDataset> foods){
 		for(CanadaFoodGuideDataset food:foods){
 			prepare(food);
