36960d9ce33d7e96c14079d301e7a039671cc28b
rsanchej <raul.sanchez@canada.ca>
2017-07-26 11:28:02 -0400

    Changed existing classification module from one-ruleset to multiple-ruleset service: * defined how to qualify a ruleset from its various components (.xls files) * replaced declarative kmodule.xml by defining KieModule programmatically * alongside the existing API, used the appropriate API to load this new ruleset * acquired a collection/List<RuleSet> from these rulesets * inserted this collection into a separate database in MongoDB in order to acquire unique id’s along with other properties and provide these in a REST service * created REST service that provides a collection of rulesets with high-level information essentially making this a multiple-rulesets service

--- src/main/java/ca/gc/ip346/classification/resource/ClassificationResource.java
+++ src/main/java/ca/gc/ip346/classification/resource/ClassificationResource.java
@@ -28,7 +28,7 @@ import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.ReleaseId;
 import org.kie.api.builder.model.KieBaseModel;
 import org.kie.api.builder.model.KieModuleModel;
-import org.kie.api.builder.model.KieSessionModel;
+// import org.kie.api.builder.model.KieSessionModel;
 import org.kie.api.runtime.KieContainer;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -63,25 +63,25 @@ public class ClassificationResource {
 		collection  = mongoClient.getDatabase(MongoClientFactory.getDatabase()).getCollection(MongoClientFactory.getCollection());
 		logger.error("[01;03;31m" + "mongo connectivity test: " + mongoClient.getDB(MongoClientFactory.getDatabase()).command("buildInfo").getString("version") + "[00;00m");
 
-		rules                   = new ArrayList<String>();
-		KieServices ks          = KieServices.Factory.get();
+		rules                         = new ArrayList<String>();
+		KieServices ks                = KieServices.Factory.get();
+		KieModuleModel kieModuleModel = ks.newKieModuleModel();
 
-		KieModuleModel kieModuleModel    = ks.newKieModuleModel();
-		KieBaseModel kieBaseModel1       = kieModuleModel.newKieBaseModel("dtables.refamt");
-		KieBaseModel kieBaseModel2       = kieModuleModel.newKieBaseModel("dtables.fop");
-		KieBaseModel kieBaseModel3       = kieModuleModel.newKieBaseModel("dtables.shortcut");
-		KieBaseModel kieBaseModel4       = kieModuleModel.newKieBaseModel("dtables.thresholds");
-		KieBaseModel kieBaseModel5       = kieModuleModel.newKieBaseModel("dtables.init");
-		KieBaseModel kieBaseModel6       = kieModuleModel.newKieBaseModel("dtables.tier");
+		KieBaseModel kieBaseModel1 = kieModuleModel.newKieBaseModel("dtables.refamt")     .addPackage("dtables.refamt")     ;
+		KieBaseModel kieBaseModel2 = kieModuleModel.newKieBaseModel("dtables.fop")        .addPackage("dtables.fop")        ;
+		KieBaseModel kieBaseModel3 = kieModuleModel.newKieBaseModel("dtables.shortcut")   .addPackage("dtables.shortcut")   ;
+		KieBaseModel kieBaseModel4 = kieModuleModel.newKieBaseModel("dtables.thresholds") .addPackage("dtables.thresholds") ;
+		KieBaseModel kieBaseModel5 = kieModuleModel.newKieBaseModel("dtables.init")       .addPackage("dtables.init")       ;
+		KieBaseModel kieBaseModel6 = kieModuleModel.newKieBaseModel("dtables.tier")       .addPackage("dtables.tier")       ;
 
-		/* KieSessionModel kieSessionModel1 = */ kieBaseModel1.newKieSessionModel("ksession-process-refamt");
-		/* KieSessionModel kieSessionModel2 = */ kieBaseModel2.newKieSessionModel("ksession-process-fop");
-		/* KieSessionModel kieSessionModel3 = */ kieBaseModel3.newKieSessionModel("ksession-process-shortcut");
-		/* KieSessionModel kieSessionModel4 = */ kieBaseModel4.newKieSessionModel("ksession-process-thresholds");
-		/* KieSessionModel kieSessionModel5 = */ kieBaseModel5.newKieSessionModel("ksession-process-init");
-		/* KieSessionModel kieSessionModel6 = */ kieBaseModel6.newKieSessionModel("ksession-process-tier");
+		/* KieSessionModel kieSessionModel1 = */ kieBaseModel1.newKieSessionModel("ksession-process-refamt")     ;
+		/* KieSessionModel kieSessionModel2 = */ kieBaseModel2.newKieSessionModel("ksession-process-fop")        ;
+		/* KieSessionModel kieSessionModel3 = */ kieBaseModel3.newKieSessionModel("ksession-process-shortcut")   ;
+		/* KieSessionModel kieSessionModel4 = */ kieBaseModel4.newKieSessionModel("ksession-process-thresholds") ;
+		/* KieSessionModel kieSessionModel5 = */ kieBaseModel5.newKieSessionModel("ksession-process-init")       ;
+		/* KieSessionModel kieSessionModel6 = */ kieBaseModel6.newKieSessionModel("ksession-process-tier")       ;
 
-		KieFileSystem kfs                = ks.newKieFileSystem();
+		KieFileSystem kfs = ks.newKieFileSystem();
 		kfs.writeKModuleXML(kieModuleModel.toXML());
 		this.releaseId = ks.newKieBuilder(kfs).buildAll().getKieModule().getReleaseId();
 
@@ -93,55 +93,120 @@ public class ClassificationResource {
 
 		logger.error("[01;03;31m" + "list out the complete set of rulesets:\n" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(kContainer.getKieBaseNames()) + "[00;00m");
 
+		/**
+		 * six rulesets get created - this is arbitrary
+		 */
 		for (String kieBaseName : kContainer.getKieBaseNames()) {
-			logger.error("[01;03;31m\n" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(kContainer.getKieSessionNamesInKieBase(kieBaseName)) + "[00;00m");
+			logger.error("[01;03;31m" + "session name:\n" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(kContainer.getKieSessionNamesInKieBase(kieBaseName)) + "[00;00m");
 			for (String session : kContainer.getKieSessionNamesInKieBase(kieBaseName)) {
 				String rule = session.replaceAll(pattern, "$1");
 				rules.add(rule);
 			}
-			break;
+			// break;
 		}
 
 		logger.error("[01;03;31m" + "distinct rules:\n" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(rules) + "[00;00m");
 
-		// Check to see if rulesets' identifiers exist in MongoDB and overwrite them if they don't
+		// Check to see if rulesets' identifiers exist in MongoDB and create them if they don't
 
 		MongoCursor<Document> cursorDocMap = null;
 		List<String> ids = new ArrayList<String>();
 
-		for (String rule : rules) {
-			Boolean isNotValidRulesetId = true;
+		if (collection.count() == 0) {
+			Integer ruleSetCounter = 0;
+			for (String rule : rules) {
+				Boolean isNotValidRulesetId = true;
 
-			if (ObjectId.isValid(rule)) {
-				System.out.println("[01;31m" + "Valid hexadecimal representation of RulesetId " + rule + "[00;00m");
+				if (ObjectId.isValid(rule)) {
+					System.out.println("[01;31m" + "Valid hexadecimal representation of RulesetId " + rule + "[00;00m");
 
-				cursorDocMap = collection.find(new Document("_id", new ObjectId(rule))).iterator();
-				if (!cursorDocMap.hasNext()) {
-					// create new item and replace current rule value with new _id
-					isNotValidRulesetId = false;
+					cursorDocMap = collection.find(new Document("_id", new ObjectId(rule))).iterator();
+					if (!cursorDocMap.hasNext()) {
+						// always the case - so create all arbitrary rulesets
+						// create new item and replace current rule value with new _id
+						isNotValidRulesetId = false;
+					}
+				}
+
+				if (isNotValidRulesetId) {
+					++ruleSetCounter;
+					logger.error("[01;03;31m" + ruleSetCounter + "[00;00m");
+					Document doc = new Document()
+						.append("name",     "Ruleset " + ruleSetCounter.toString() + "")
+						.append("isProd",   ruleSetCounter == 1 ? true : false)
+						.append("location", null);
+					collection.insertOne(doc);
+					ObjectId id = (ObjectId)doc.get("_id");
+					collection.updateOne(
+							eq("_id", id),
+							combine(
+								set("isProd",   ruleSetCounter == 1 ? true : false),
+								currentDate("modifiedDate"))
+							);
+					ids.add(rules.indexOf(rule), id.toString());
+				} else {
+					ids.add(rules.indexOf(rule), rule); // never happens?
 				}
 			}
-
-			if (isNotValidRulesetId) {
-				Document doc = new Document()
-					.append("name",     null)
-					.append("isProd",   null)
-					.append("location", null);
-				collection.insertOne(doc);
+		} else {
+			cursorDocMap = collection.find().iterator();
+			while (cursorDocMap.hasNext()) {
+				Document doc = cursorDocMap.next();
 				ObjectId id = (ObjectId)doc.get("_id");
-				collection.updateOne(
-						eq("_id", id),
-						combine(
-							set("name", null),
-							currentDate("modifiedDate"))
-						);
-				ids.add(rules.indexOf(rule), id.toString());
-			} else {
-				ids.add(rules.indexOf(rule), rule);
+				ids.add(id.toString());
 			}
 		}
 
-		logger.error("[01;03;31m\n" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(ids) + "[00;00m");
+		logger.error("[01;03;31m" + "ruleset identifier(s):\n" + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(ids) + "[00;00m");
+
+		List<String> categories = new ArrayList<String>();
+		categories.add("refamt");
+		categories.add("fop");
+		categories.add("shortcut");
+		categories.add("thresholds");
+		categories.add("init");
+		categories.add("tier");
+
+		kieBaseModel1.removeKieSessionModel("ksession-process-refamt");
+		kieBaseModel2.removeKieSessionModel("ksession-process-fop");
+		kieBaseModel3.removeKieSessionModel("ksession-process-shortcut");
+		kieBaseModel4.removeKieSessionModel("ksession-process-thresholds");
+		kieBaseModel5.removeKieSessionModel("ksession-process-init");
+		kieBaseModel6.removeKieSessionModel("ksession-process-tier");
+
+		ks             = KieServices.Factory.get();
+		kieModuleModel = ks.newKieModuleModel();
+
+		kieBaseModel1 = kieModuleModel.newKieBaseModel("dtables.refamt")     .addPackage("dtables.refamt")     ;
+		kieBaseModel2 = kieModuleModel.newKieBaseModel("dtables.fop")        .addPackage("dtables.fop")        ;
+		kieBaseModel3 = kieModuleModel.newKieBaseModel("dtables.shortcut")   .addPackage("dtables.shortcut")   ;
+		kieBaseModel4 = kieModuleModel.newKieBaseModel("dtables.thresholds") .addPackage("dtables.thresholds") ;
+		kieBaseModel5 = kieModuleModel.newKieBaseModel("dtables.init")       .addPackage("dtables.init")       ;
+		kieBaseModel6 = kieModuleModel.newKieBaseModel("dtables.tier")       .addPackage("dtables.tier")       ;
+
+		List<KieBaseModel> bases = new ArrayList<KieBaseModel>();
+
+		bases.add(kieBaseModel1); // refamt
+		bases.add(kieBaseModel2); // fop
+		bases.add(kieBaseModel3); // shortcut
+		bases.add(kieBaseModel4); // thresholds
+		bases.add(kieBaseModel5); // init
+		bases.add(kieBaseModel6); // tier
+
+		for (int j = 0; j < categories.size(); ++j) {
+			for (int i = 0; i < ids.size(); ++i) {
+				bases.get(j).newKieSessionModel("ksession-process-" + ids.get(i) + "-" + categories.get(j));
+			}
+		}
+
+		kfs = ks.newKieFileSystem();
+		kfs.writeKModuleXML(kieModuleModel.toXML());
+
+		logger.error("[01;03;31m" + "\n" + (kieModuleModel.toXML()) + "[00;00m");
+
+		this.releaseId = ks.newKieBuilder(kfs).buildAll().buildAll().getKieModule().getReleaseId();
+
+		this.rules = ids;
 	}
 
 	// @OPTIONS
@@ -189,9 +254,25 @@ public class ClassificationResource {
 	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 	@JacksonFeatures(serializationEnable = {SerializationFeature.INDENT_OUTPUT})
 	public Map<String, Object> getRulesets() {
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("rules", rules);
-		return map;
+		Map<String, Object> result = new HashMap<String, Object>();
+		List<Map<String, Object>> rulesets = new ArrayList<Map<String, Object>>();
+		MongoCursor<Document> cursorDocMap = null;
+		for (String rule : rules) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			cursorDocMap = collection.find(new Document("_id", new ObjectId(rule))).iterator();
+			while (cursorDocMap.hasNext()) {
+				Document doc = cursorDocMap.next();
+				ObjectId id = (ObjectId)doc.get("_id");
+				map.put("id", id.toString());
+				String name = (String)doc.get("name");
+				map.put("name", name);
+				Boolean isProd = (Boolean)doc.get("isProd");
+				map.put("isProd", isProd);
+				rulesets.add(map);
+			}
+		}
+		result.put("rulesets", rulesets);
+		return result;
 	}
 
 	@DELETE
