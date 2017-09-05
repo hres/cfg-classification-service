91c2c65c5dd4d1e4e1a4cace8c647074610d0ced
rsanchej <raul.sanchez@canada.ca>
2017-09-05 15:01:29 -0400

    Added ruleset count to debug HRE

--- src/main/java/ca/gc/ip346/classification/resource/ClassificationResource.java
+++ src/main/java/ca/gc/ip346/classification/resource/ClassificationResource.java
@@ -65,6 +65,7 @@ public class ClassificationResource {
 	public ClassificationResource() {
 		mongoClient = MongoClientFactory.getMongoClient();
 		collection  = mongoClient.getDatabase(MongoClientFactory.getDatabase()).getCollection(MongoClientFactory.getCollection());
+		logger.debug("[01;03;31m" + "collection: " + new GsonBuilder().setDateFormat("yyyy-MM-dd").setPrettyPrinting().create().toJson(collection.count()) + "[00;00m");
 		logger.debug("[01;03;31m" + "new mongo connectivity test: " + mongoClient.getDatabase(MongoClientFactory.getDatabase()).runCommand(new Document("buildInfo", 1)).getString("version") + "[00;00m");
 
 		rules                         = new ArrayList<String>();
